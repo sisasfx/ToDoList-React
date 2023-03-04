39e01dd9ab75ef87e6f239d7bb60824be33dc9fb
Miquel Ortiz Graupera <sisasfx@gmail.com>
2023-03-04 12:59:42 +0000

    feat:implementing delete item

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -5,17 +5,27 @@ import ToDoList from "./toDoList.jsx";
 //create your first component
 const Home = () => {
 
+	
+
 	const [toDo, setToDo] = useState([])
 
 	const handleToDo = (addToDo) => {
 			setToDo([...toDo, addToDo])
 	}
 
+	const deleteToDo = (itemList) => {
+		console.log(itemList)
+		for(let i = 0; i < toDo.length; i++){
+			itemList === toDo[i] ? toDo.splice(0,i) : null
+		}
+		console.log(toDo)
+	}
+
 	return (
 		<div className="container">
 			<Form handleToDo={handleToDo}/>
 			{
-				toDo.map(item => <ToDoList toDo={item}/>)
+				toDo.map(item => <ToDoList toDo={item} deleteToDo={deleteToDo}/>)
 			}
 		</div>
 	);
--- src/js/component/toDoList.jsx
+++ src/js/component/toDoList.jsx
@@ -2,11 +2,17 @@ import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {faXmark} from "@fortawesome/free-solid-svg-icons"
 
-const ToDoList = ({toDo}) => {
+const ToDoList = (props) => {
+
+    const deleteItem = () => {
+        console.log("DELETE ITEM")
+        props.deleteToDo(props.toDo)
+    }
+
     return(
         <div className="card">
-            <h5>{toDo}</h5>
-            <FontAwesomeIcon icon={faXmark} />
+            <h5 className={props.toDo}>{props.toDo}</h5>
+            <FontAwesomeIcon icon={faXmark} id="icon" onClick={deleteItem}/>
         </div>        
     );
 }
